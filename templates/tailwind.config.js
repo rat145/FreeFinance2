ace57dfcb44460c1c8a5336cd715ea02d0403d3d
rat145 <ratannn21@gmail.com>
2025-02-15 13:08:08 +0530

    Login page complete

---
+++ templates/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Scans any nested HTML files inside templates/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
